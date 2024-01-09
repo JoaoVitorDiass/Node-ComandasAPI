71c2c4a4966063b060fa6909df71bdb8bba89656
JoaoVitorDiass <joovitordiasdasilva@gmail.com>
2024-01-08 21:41:49 -0300

    feat: re-estruturação da API

--- src/app.js
+++ src/app.js
@@ -6,35 +6,14 @@ const bodyParser = require('body-parser');
 const app = express();
 const router = express.Router();
 
+// Carrega as Rotas
+const index = require('./routes/index');
+const product = require('./routes/product');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const route = router.get('/', (req, res, next) => {
-    res.status(200).send({
-        title: "Node Store API",
-        version: "0.0.1"
-    });
-});
-
-const create = router.post('/', (req, res, next) => {
-    res.status(201).send(req.body);
-});
-
-const put = router.put('/:id', (req, res, next) => {
-    let id = req.params.id;
-    res.status(201).send({
-        id: id,
-        body: req.body
-    });
-});
-
-const del = router.delete('/:id', (req, res, next) => {
-    res.status(200).send("deletado");
-});
-
-app.use('/', route);
-app.use('/', create);
-app.use('/', put);
-app.use('/', del);
+app.use('/', index);
+app.use('/', product);
 
 module.exports = app;
