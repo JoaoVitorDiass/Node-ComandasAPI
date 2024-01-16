0464371a03f3b24c3c7f1f41287fc1e9b6f19545
JoaoVitorDiass <joovitordiasdasilva@gmail.com>
2024-01-15 22:32:33 -0300

    fix:  tornando as funcoes assincronas

--- src/repositories/ProductRepository.js
+++ src/repositories/ProductRepository.js
@@ -3,44 +3,41 @@
 const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
 
-exports.get = () => {
-    return Product
-                .find({
-                    active: true
-                }, 'title price slug');
+exports.get = async() => {
+    const res = await Product.find({
+        active: true
+    }, 'title price slug');
+    return res;
 }
 
-exports.getBySlug = (slug) => {
-    return Product
-            .findOne(
-                { 
-                    slug: slug,
-                    active: true
-                },
-                'title description price slug tags'
-            )
+exports.getBySlug = async(slug) => {
+    const res = await Product.findOne({ 
+        slug: slug,
+        active: true
+    },'title description price slug tags' )
+    return res;
 }
 
-exports.getById = (id) => {
-    return Product.findById(id);
+exports.getById = async(id) => {
+    const res = await Product.findById(id);
+    return res;
 }
 
-exports.getByTag = (tag) => {
-    return Product
-        .find( {
-            tags: tag,
-            active: true
-        })
+exports.getByTag = async(tag) => {
+    const res = await Product.find({
+        tags: tag,
+        active: true
+    });
+    return res;
 }
 
-exports.create = (data) => {
+exports.create = async(data) => {
     var product = new Product(data);
-    return product.save()
+    await product.save()
 }
 
-exports.update = (id, data) => {
-
-    return Product
+exports.update = async(id, data) => {
+    await Product
             .findByIdAndUpdate(id, {
                 $set: {
                     title: data.title,
@@ -50,7 +47,7 @@ exports.update = (id, data) => {
             })
 }
 
-exports.create = (id) => {
-    return Product
+exports.create = async(id) => {
+    await Product
         .findOneAndDelete(id)
 }
