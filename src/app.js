21d0f0ce728f6059bd5b77d60b3c0144552eb1e6
JoaoVitorDiass <joovitordiasdasilva@gmail.com>
2024-01-16 22:40:14 -0300

    feat: envio de email

--- src/app.js
+++ src/app.js
@@ -3,12 +3,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const config = require('./config');
 
 const app = express();
 const router = express.Router();
 
 // conecta ao banco
-mongoose.connect('mongodb+srv://joovitordiasdasilva:B4uCuVWiZyloeOUy@ndstr.qcbuolg.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect(config.connectionString);
 
 // carrega as models
 const Product = require("./models/product");
---
+++ src/config.js
@@ -0,0 +1,8 @@
+global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
+global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store!';
+
+module.exports = {
+    connectionString: 'mongodb+srv://joovitordiasdasilva:B4uCuVWiZyloeOUy@ndstr.qcbuolg.mongodb.net/?retryWrites=true&w=majority',
+    sendgridKey: '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',
+    containerConnectionString: 'SUA CONNECTION STRING'
+}
